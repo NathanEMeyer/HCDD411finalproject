1291dffa9645a992006c213bf98a12a459cd9cdc
Nathan Meyer <nemc3po@gmail.com>
2026-04-18 19:20:24 -0400

    Add files via upload

---
+++ FinalProject/src/main/java/psu/edu/FinalProject/Service/EmployeeService.java
@@ -0,0 +1,17 @@
+package psu.edu.FinalProject.Service;
+
+import java.util.List;
+
+import psu.edu.FinalProject.entity.Employee;
+
+public interface EmployeeService {
+	
+	List<Employee> findAll();
+	
+	Employee findById(int theId);
+	
+	Employee save(Employee theEmployee);
+	
+	void deleteById(int theId);
+
+}
